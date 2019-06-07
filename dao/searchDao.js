8d4de25ef7ea3805acb582ca8858cb2b0500f542
Salem <SOzaki19@cornellcollege.edu>
2019-06-07 14:15:39 -0400

    improve doc for searchDao

--- dao/searchDao.js
+++ dao/searchDao.js
@@ -1,7 +1,7 @@
 const db = require('./db');
 
 exports.getAll = function(cb) {
-    // filter out reserver because we want tickets that have not been taken yet
+    // filter out reserver by default because we want tickets that have not been taken yet
     db.query('SELECT * FROM tbl_tickets WHERE reserver IS NULL;', function(err, result) {
         cb(err, result);
     });
