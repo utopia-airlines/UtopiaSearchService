d42dcf5d700905fec84085ebc218be353476b6c5
Salem <SOzaki19@cornellcollege.edu>
2019-06-07 17:46:49 -0400

    add class filter

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,10 +2,18 @@ const searchDao = require('../dao/searchDao');
 const routes = require('express').Router();
 
 routes.get('/', function(req, res) {
-    searchDao.getAll(function(error, result) {
-        if(error) res.status(500).send("Sorry, but something happend on our server!");
-        res.setHeader('Content-Type', 'application/json');
-        res.send(result);
+    // create a clone of the request query
+    const filter = {};
+    Object.assign(filter, req.query);
+
+    searchDao.getAll(filter, function(error, result) {
+        if(error) {
+            console.log(error);
+            res.status(500).send("Sorry, but something happend on our server!");
+        } else {
+            res.setHeader('Content-Type', 'application/json');
+            res.send(result);
+        }
     });
 });
 
