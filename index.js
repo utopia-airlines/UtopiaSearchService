91ae3eaf5a6c41d30cede65577e3fe1d223fa115
Salem <SOzaki19@cornellcollege.edu>
2019-06-07 13:57:41 -0400

    basic index and package-lock

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(require('./controllers/searchController'));
+
+app.listen(8080);
+console.log('Server running in port: 8080 ...')
