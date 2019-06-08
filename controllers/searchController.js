6f4c463582af26907de2a95bf194fc7c1011233c
Salem <SOzaki19@cornellcollege.edu>
2019-06-08 18:06:59 -0400

    pass request query directly into the dao

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -2,12 +2,9 @@ const searchDao = require('../dao/searchDao');
 const routes = require('express').Router();
 
 routes.get('/', function(req, res) {
-    // create a clone of the request query
-    const filter = {};
-    Object.assign(filter, req.query);
 
-    searchDao.getAll(filter, function(error, result) {
-        if(error) {
+    searchDao.getAll(req.query, function(error, result) {
+        if (error) {
             console.log(error);
             res.status(500).send('Sorry, but something happend on our server!');
         } else {
