b4829dfde1dce6f51d8d685289e37daa7f12294d
Salem <SOzaki19@cornellcollege.edu>
2019-06-13 18:31:03 -0400

    basic index test (integration)
    Added all of the necessary dependencies, and changes to index and dao to get tests to work with an in-memory database (sqlite) using http client calls

--- dao/searchDao.js
+++ dao/searchDao.js
@@ -1,4 +1,5 @@
 const db = require('./db');
+const sqliteDatabase = require('better-sqlite3');
 
 exports.getAll = function(filter, cb) {
     let classFilter = '';
@@ -6,6 +7,15 @@ exports.getAll = function(filter, cb) {
         classFilter = ' AND class = ' + db.escape(filter.class);
     }
     let sqlQuery = 'SELECT * FROM tbl_tickets WHERE reserver IS NULL' + classFilter + ';';
-    // filter out reserver by default because we want tickets that have not been taken yet
-    db.query(sqlQuery,cb);
+
+    if(process.env.NODE_ENV === 'test') {
+        // for debugging add `verbose: console.log` to the object being passed in eg. {verbose: console.log, memory: true} (also look at the README)
+        const Testdb = new sqliteDatabase('airlinesTest.db', {memory: true});
+        const result = Testdb.prepare(sqlQuery).all();
+        // console.log(result);
+        cb(null, result);
+    } else {
+        // filter out reserver by default because we want tickets that have not been taken yet
+        db.query(sqlQuery,cb);
+    }
 };
