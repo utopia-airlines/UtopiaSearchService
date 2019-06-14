1906ff72c6124652a256c3ed638df51fe4631716
Salem <SOzaki19@cornellcollege.edu>
2019-06-14 16:59:14 -0400

    add seat filter
    Now you can add a seat filter for you query parameter

--- dao/searchDao.js
+++ dao/searchDao.js
@@ -7,7 +7,28 @@ exports.getAll = function(filter, cb) {
     if(filter.class) {
         classFilter = ' AND class = ' + db.escape(filter.class);
     }
-    let sqlQuery = 'SELECT * FROM tbl_tickets WHERE reserver IS NULL' + classFilter + ';';
+
+    // filter to be used on sql query
+    let seatFilter = '';
+    let seatArray = null;
+    // still in string form after getting passed as a query parameter
+    // if there is something the seat object
+    if(filter.seat) {seatArray = filter.seat.split(',');}
+
+    if(Array.isArray(seatArray)) {
+        let i;
+        let seatLength = seatArray.length;
+        let sqlSeatList = '';
+        for (i = 0; i < seatLength; i++) {
+            let comma = ',';
+            // last element will not have a comma after it
+            if(i === seatLength - 1) {comma = '';}
+            sqlSeatList = sqlSeatList + db.escape(seatArray[i]) + comma;
+        }
+        seatFilter = ' AND seat IN (' + sqlSeatList + ')';
+    }
+    let sqlQuery = 'SELECT * FROM tbl_tickets WHERE reserver IS NULL' + classFilter +
+        seatFilter + ';';
 
     if(process.env.NODE_ENV === 'test') {
         // Please look at the README for debugging to the console
