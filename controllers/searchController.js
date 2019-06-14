7bc937ff448983edf0e9460b739496ed4f29059c
Salem <SOzaki19@cornellcollege.edu>
2019-06-14 11:22:09 -0400

    add logger to controller
    The reason I did not add it anywhere else is because most of them it is not necessary to track. In addition the controller is the only one to expect an error if thrown

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,15 +1,28 @@
 const searchDao = require('../dao/searchDao');
 const routes = require('express').Router();
 
+const winston = require('winston');
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.json(),
+    defaultMeta: { service: 'searchController' },
+    transports: [
+    //   new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'combined.log' })
+    ]
+});
+
 routes.get('/', function(req, res) {
 
     searchDao.getAll(req.query, function(error, result) {
         if (error) {
+            logger.error(error);
             console.log(error);
             res.status(500).send('Sorry, but something happend on our server!');
         } else {
             res.setHeader('Content-Type', 'application/json');
             res.send(result);
+            logger.info('Succesfully sent', result);
         }
     });
 });
