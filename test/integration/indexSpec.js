a91989802eac0a8694e63f84d905eaae20f0fdf6
Salem <SOzaki19@cornellcollege.edu>
2019-06-14 19:42:53 -0400

    variables are now camelcase

--- test/integration/indexSpec.js
+++ test/integration/indexSpec.js
@@ -104,8 +104,8 @@ describe('All with queries', () => {
         it('should GET a list tickets with departure date between 2038-01-11 00:00:00 and 2038-01-12 00:00:00', (done) => {
             chai.request(server)
                 .get('/')
-                .query({departure_date_after: '2038-01-11 00:00:00',
-                        departure_date_before: '2038-01-12 00:00:00'}) // /?departure_date_after=2038-01-11 00:00:00&departure_date_before=2038-01-12 00:00:00
+                .query({departureDateAfter: '2038-01-11 00:00:00',
+                        departureDateBefore: '2038-01-12 00:00:00'}) // /?departureDateAfter=2038-01-11 00:00:00&departureDateBefore=2038-01-12 00:00:00
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('array');
@@ -117,7 +117,7 @@ describe('All with queries', () => {
         it('should GET a list tickets with departure date between a non given datetime and 2038-01-12 00:00:00', (done) => {
             chai.request(server)
                 .get('/')
-                .query({departure_date_before: '2038-01-12 00:00:00'}) // /?departure_date_before=2038-01-12 00:00:00
+                .query({departureDateBefore: '2038-01-12 00:00:00'}) // /?departureDateBefore=2038-01-12 00:00:00
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('array');
@@ -129,7 +129,7 @@ describe('All with queries', () => {
         it('should GET a list tickets with departure date between 2038-01-11 00:00:00 and a non given datetime', (done) => {
             chai.request(server)
                 .get('/')
-                .query({departure_date_after: '2038-01-11 00:00:00'}) // /?departure_date_after=2038-01-11 00:00:00
+                .query({departureDateAfter: '2038-01-11 00:00:00'}) // /?departureDateAfter=2038-01-11 00:00:00
                 .end((err, res) => {
                     res.should.have.status(200);
                     res.body.should.be.a('array');
