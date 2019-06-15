1d269cc03fc5f17b853219d53f82d95c67385eeb
Salem <SOzaki19@cornellcollege.edu>
2019-06-15 18:57:49 -0400

    move class filter to external file
    to util/classFilter file.

---
+++ util/classFilter.js
@@ -0,0 +1,8 @@
+// pass in the class number and the db connection (which includes the escapte function)
+module.exports.classFilter = (classNum, db) => {
+    let classFilter = '';
+    if(classNum) {
+        classFilter = ' AND class = ' + db.escape(classNum);
+    }
+    return classFilter;
+}
