6f5924ed5085ec0d688201ead992d90331b0b61a
Salem <SOzaki19@cornellcollege.edu>
2019-06-15 19:09:30 -0400

    move seat filter to external file
    to util/seatFilter file.

--- dao/searchDao.js
+++ dao/searchDao.js
@@ -1,31 +1,14 @@
 const db = require('./db');
 const classFilterFunction = require('../util/classFilter');
+const seatFilterFunction = require('../util/seatFilter');
 let sqliteDatabase;
 if(process.env.NODE_ENV === 'test') {sqliteDatabase = require('better-sqlite3');}
 
 exports.getAll = function(filter, cb) {
     // get class filter
     const classFilter = classFilterFunction.classFilter(filter.class, db);
-
-    // filter to be used on sql query
-    let seatFilter = '';
-    let seatArray = null;
-    // still in string form after getting passed as a query parameter
-    // if there is something the seat object
-    if(filter.seat) {seatArray = filter.seat.split(',');}
-
-    if(Array.isArray(seatArray)) {
-        let i;
-        let seatLength = seatArray.length;
-        let sqlSeatList = '';
-        for (i = 0; i < seatLength; i++) {
-            let comma = ',';
-            // last element will not have a comma after it
-            if(i === seatLength - 1) {comma = '';}
-            sqlSeatList = sqlSeatList + db.escape(seatArray[i]) + comma;
-        }
-        seatFilter = ' AND seat IN (' + sqlSeatList + ')';
-    }
+    // get seat filter
+    const seatFilter = seatFilterFunction.seatFilter(filter.seat, db);
 
     let departureDateFilter = '';
     let departureDateAfter = '\'0000-00-00 00:00:00\'';
