1bb7d3153c17c55d54288e607dec13ab5dca547b
Salem <SOzaki19@cornellcollege.edu>
2019-06-15 20:32:32 -0400

    have errors go into an additional file
    Having errors go to both the combined and error logs so if somethings happends, it would easier to find what the error was

--- util/controllerLogger.js
+++ util/controllerLogger.js
@@ -5,7 +5,7 @@ module.exports.createLogger = winston.createLogger({
     format: winston.format.json(),
     defaultMeta: { service: 'searchController' },
     transports: [
-    //   new winston.transports.File({ filename: 'error.log', level: 'error' }),
+      new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.File({ filename: 'combined.log' })
     ]
 });
