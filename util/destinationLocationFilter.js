fa417370399ab746f4ed1f8056f5c5da70ebf278
Salem <SOzaki19@cornellcollege.edu>
2019-06-16 18:19:04 -0400

    moved code to outside file
    moved the creation of sql list string to an outside file that I use

--- util/destinationLocationFilter.js
+++ util/destinationLocationFilter.js
@@ -1,18 +1,12 @@
+const listLocationSQL = require('./listLocationsForSQL');
 // pass in the destination location list and the db connection (which includes the escapte function)
 module.exports.destinationLocationFilter = (destinationLocation, db) => {
     let destinationLocationFilter = ''; // filter to be used on sql query
     let destinationLocationArray = null;
     if(destinationLocation) {destinationLocationArray = destinationLocation.split(',');} // still in string form after getting passed as a query parameter if there is something the destination_location object
     if(Array.isArray(destinationLocationArray)) {
-        let i;
-        let destinationLocationLength = destinationLocationArray.length;
-        let sqldestinationLocationList = '';
-        for (i = 0; i < destinationLocationLength; i++) {
-            let comma = ',';
-            if(i === destinationLocationLength - 1) {comma = '';}// last element will not have a comma after it
-            sqldestinationLocationList = sqldestinationLocationList + db.escape(destinationLocationArray[i]) + comma;
-        }
-        destinationLocationFilter = ' AND destination IN (' + sqldestinationLocationList + ')';
+        let sqlDestinationLocationList = listLocationSQL(destinationLocationArray, db);
+        destinationLocationFilter = ' AND destination IN (' + sqlDestinationLocationList + ')';
     }
     return destinationLocationFilter;
 };
