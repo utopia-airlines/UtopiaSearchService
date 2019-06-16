24c99f005f65e8d3afb956a01a8527569b78fb3e
Salem <SOzaki19@cornellcollege.edu>
2019-06-16 18:37:24 -0400

    condensed line size
    Simply moved the comments after the item it was commenting because they were both short enough

--- util/seatFilter.js
+++ util/seatFilter.js
@@ -1,8 +1,7 @@
 const listLocationSQL = require('./listForSQL');
 // pass in the seat name list and the db connection (which includes the escapte function)
 module.exports.seatFilter = (seat, db) => {
-    // filter to be used on sql query
-    let seatFilter = '';
+    let seatFilter = ''; // filter to be used on sql query
     let seatArray = null;
     // still in string form after getting passed as a query parameter
     // if there is something the seat object
