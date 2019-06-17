e0c47b4f4aeb01b8dc2ec0ab3abe5f53ab1eb792
Salem <SOzaki19@cornellcollege.edu>
2019-06-17 17:43:36 -0400

    change undefined to null
    making changes based off of codacy's guidelines.

--- test/unit/departureDateFilterSpec.js
+++ test/unit/departureDateFilterSpec.js
@@ -12,13 +12,13 @@ describe('departure date filter file', () => {
         });
 
         it('should return a string that will filter out tickets with an arrival date between 0000-00-00 00:00:00 and 2038-01-12 00:00:00', (done) => {
-            let filterObj = departureDateFilter(undefined, '2038-01-12 00:00:00', db);
+            let filterObj = departureDateFilter(null, '2038-01-12 00:00:00', db);
             filterObj.should.equal(' AND departure_date BETWEEN \'0000-00-00 00:00:00\' AND \'2038-01-12 00:00:00\'');
             done();
         });
 
         it('should return a string that will filter out tickets with arrival date between 2038-01-11 00:00:00 and 9999-12-31 23:59:59', (done) => {
-            let filterObj = departureDateFilter('2038-01-11 00:00:00', undefined, db);
+            let filterObj = departureDateFilter('2038-01-11 00:00:00', null, db);
             filterObj.should.equal(' AND departure_date BETWEEN \'2038-01-11 00:00:00\' AND \'9999-12-31 23:59:59\'');
             done();
         });
