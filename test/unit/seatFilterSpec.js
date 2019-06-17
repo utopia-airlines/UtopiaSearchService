ad09f6dddc0d044ba05eab3d8b8a7ab9577e0e8d
Salem <SOzaki19@cornellcollege.edu>
2019-06-16 20:35:47 -0400

    unit test for seat filter

---
+++ test/unit/seatFilterSpec.js
@@ -0,0 +1,26 @@
+const should = require('chai').should();
+const db = require('../../dao/db');
+
+const seatFilter = require('../../util/seatFilter').seatFilter;
+
+describe('seat filter file', () => {
+    describe('filtering function', () => {
+        it('should return a string that will filter out tickets with seat "A"', (done) => {
+            let filterObj = seatFilter('A', db);
+            filterObj.should.equal(' AND seat IN (\'A\')');
+            done();
+        });
+
+        it('should return a string that will filter out tickets with seat "A" or "B"', (done) => {
+            let filterObj = seatFilter('A,B', db);
+            filterObj.should.equal(' AND seat IN (\'A\',\'B\')');
+            done();
+        });
+
+        it('should return a string that will filter out tickets with seat "Nothing"', (done) => {
+            let filterObj = seatFilter('Nothing', db);
+            filterObj.should.equal(' AND seat IN (\'Nothing\')');
+            done();
+        });
+    });
+});
