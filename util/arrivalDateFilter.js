f11e5095ef0b75c8282f4650197721e5a3397f65
Salem <SOzaki19@cornellcollege.edu>
2019-06-17 12:42:35 -0400

    removed unnecessary code
    Simply returned an empty string or not if there is something that needs to be filtered (thank you @kingjon3377)

--- util/arrivalDateFilter.js
+++ util/arrivalDateFilter.js
@@ -1,7 +1,6 @@
 // pass in the arrival date after a certain date (optional) and before a certain date (optional),
 // and the db connection (which includes the escape function)
 module.exports.arrivalDateFilter = (arrivalDateAfter, arrivalDateBefore, db) => {
-    let arrivalDateFilter = '';
     // if the user wants to arrive anytime after `arrivalDateAfter`
     arrivalDateAfter = arrivalDateAfter ? db.escape(arrivalDateAfter) : '\'0000-00-00 00:00:00\'';
 
@@ -9,7 +8,8 @@ module.exports.arrivalDateFilter = (arrivalDateAfter, arrivalDateBefore, db) => 
     arrivalDateBefore = arrivalDateBefore ? db.escape(arrivalDateBefore) : '\'9999-12-31 23:59:59\'';
 
     if (arrivalDateAfter || arrivalDateBefore) {
-        arrivalDateFilter = ' AND arrival_date BETWEEN ' + arrivalDateAfter + ' AND ' + arrivalDateBefore;
+        return ' AND arrival_date BETWEEN ' + arrivalDateAfter + ' AND ' + arrivalDateBefore;
+    } else {
+        return '';
     }
-    return arrivalDateFilter;
 };
--- util/destinationLocationFilter.js
+++ util/destinationLocationFilter.js
@@ -1,13 +1,13 @@
 const listLocationSQL = require('./listForSQL');
 // pass in the destination location list and the db connection (which includes the escape function)
 module.exports.destinationLocationFilter = (destinationLocation, db) => {
-    let destinationLocationFilter = ''; // filter to be used on sql query
     let destinationLocationArray = null;
     // still in string form after getting passed as a query parameter if there is something the destination_location object
     if (destinationLocation) {
         destinationLocationArray = destinationLocation.split(',');
         let sqlDestinationLocationList = listLocationSQL(destinationLocationArray, db);
-        destinationLocationFilter = ' AND destination IN (' + sqlDestinationLocationList + ')';
+        return ' AND destination IN (' + sqlDestinationLocationList + ')';
+    } else {
+        return '';
     }
-    return destinationLocationFilter;
 };
