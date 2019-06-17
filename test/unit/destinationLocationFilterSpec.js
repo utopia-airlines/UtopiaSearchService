d501417780ec9aacc6b01dde480de4a8ac5eebb9
Salem <SOzaki19@cornellcollege.edu>
2019-06-16 20:27:28 -0400

    unit test for destination location filter

---
+++ test/unit/destinationLocationFilterSpec.js
@@ -0,0 +1,26 @@
+const should = require('chai').should();
+const db = require('../../dao/db');
+
+const destinationLocationFilter = require('../../util/destinationLocationFilter').destinationLocationFilter;
+
+describe('destination location filter file', () => {
+    describe('filtering function', () => {
+        it('should return a string that will filter out tickets with a "FSE" destination location', (done) => {
+            let filterObj = destinationLocationFilter('FSE', db);
+            filterObj.should.equal(' AND destination IN (\'FSE\')');
+            done();
+        });
+
+        it('should return a string that will filter out tickets destination from "FSE" or "CAR"', (done) => {
+            let filterObj = destinationLocationFilter('FSE,CAR', db);
+            filterObj.should.equal(' AND destination IN (\'FSE\',\'CAR\')');
+            done();
+        });
+
+        it('should return a string that will filter out tickets destination from Nothing', (done) => {
+            let filterObj = destinationLocationFilter('Nothing', db);
+            filterObj.should.equal(' AND destination IN (\'Nothing\')');
+            done();
+        });
+    });
+});
