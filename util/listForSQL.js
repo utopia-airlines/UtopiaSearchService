2eb829c0ac4372aa0589a2fd3db036a611521183
Salem <SOzaki19@cornellcollege.edu>
2019-06-17 16:48:28 -0400

    Expected parentheses around arrow function argument
    This is the standarded and it makes more sense

--- util/listForSQL.js
+++ util/listForSQL.js
@@ -1,5 +1,5 @@
 // given an array and 
 module.exports = (array, db) => {
     // cannot abbreviate to just db.escape because it will throw an error
-    return array.map(item => db.escape(item)).join(',');
+    return array.map((item) => db.escape(item)).join(',');
 };
