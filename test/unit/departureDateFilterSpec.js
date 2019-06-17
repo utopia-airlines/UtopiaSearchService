82ef150865d0403489d687507416ad75aa9b2c79
Salem <SOzaki19@cornellcollege.edu>
2019-06-16 20:06:51 -0400

    unit test for departure date filter

---
+++ test/unit/departureDateFilterSpec.js
@@ -0,0 +1,26 @@
+const should = require('chai').should();
+const db = require('../../dao/db');
+
+const departureDateFilter = require('../../util/departureDateFilter').departureDateFilter;
+
+describe('departure date filter file', () => {
+    describe('filtering function', () => {
+        it('should return a string that will filter out tickets with an arrival date between 2038-01-11 00:00:00 and 2038-01-12 00:00:00', (done) => {
+            let filterObj = departureDateFilter('2038-01-11 00:00:00', '2038-01-12 00:00:00', db);
+            filterObj.should.equal(' AND departure_date BETWEEN \'2038-01-11 00:00:00\' AND \'2038-01-12 00:00:00\'');
+            done();
+        });
+
+        it('should return a string that will filter out tickets with an arrival date between 0000-00-00 00:00:00 and 2038-01-12 00:00:00', (done) => {
+            let filterObj = departureDateFilter(undefined, '2038-01-12 00:00:00', db);
+            filterObj.should.equal(' AND departure_date BETWEEN \'0000-00-00 00:00:00\' AND \'2038-01-12 00:00:00\'');
+            done();
+        });
+
+        it('should return a string that will filter out tickets with arrival date between 2038-01-11 00:00:00 and 9999-12-31 23:59:59', (done) => {
+            let filterObj = departureDateFilter('2038-01-11 00:00:00', undefined, db);
+            filterObj.should.equal(' AND departure_date BETWEEN \'2038-01-11 00:00:00\' AND \'9999-12-31 23:59:59\'');
+            done();
+        });
+    });
+});
