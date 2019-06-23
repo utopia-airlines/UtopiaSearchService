6feb8b109ee733c9d3ec0f230e1e9ca817620a9e
Salem <SOzaki19@cornellcollege.edu>
2019-06-23 17:22:10 -0400

    remove the get dao from returning the id of the flight

--- app/dao/searchDao.js
+++ app/dao/searchDao.js
@@ -3,7 +3,7 @@ const dbConn = require('./db');
 const db = require('../config/db.config.prod');
 exports.get = function(filter, cb) {
     // filter out reserver by default because we want tickets that have not been taken yet
-    let sqlQuery = 'SELECT flight, seat_row, seat, class, departure, destination, departure_date, arrival_date, flight_number FROM tbl_tickets AS t LEFT JOIN tbl_flights AS f ON t.flight = f.id WHERE reserver IS NULL' +
+    let sqlQuery = 'SELECT seat_row, seat, class, departure, destination, departure_date, arrival_date, flight_number FROM tbl_tickets AS t LEFT JOIN tbl_flights AS f ON t.flight = f.id WHERE reserver IS NULL' +
         allFilters.classFilter(filter.class, db) + allFilters.seatFilter(filter.seat, db) +
         allFilters.departureDateFilter(filter.departureDateAfter, filter.departureDateBefore, db) + allFilters.arrivalDateFilter(filter.arrivalDateAfter, filter.arrivalDateBefore, db) +
         allFilters.departureLocationFilter(filter.departure_location, db) + allFilters.destinationLocationFilter(filter.destination_location, db) + ';';
