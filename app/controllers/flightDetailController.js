5f37cd98163edfeb866457c70c9f31fa057147a9
Salem <SOzaki19@cornellcollege.edu>
2019-06-25 22:04:20 -0400

    use callback function instead of repeating code for flight detail

--- app/controllers/flightDetailController.js
+++ app/controllers/flightDetailController.js
@@ -1,32 +1,17 @@
 const flightDetailDao = require('../dao/flightDetailDao');
 const routes = require('express').Router();
 const logger = require('../util/logger').createLogger('flightDetailController');
+const cbFunction = require('./controllerCallBack');
 
 routes.get('/flightDetails', function(req, res) {
     flightDetailDao.get(req.query, function(error, result) {
-        if (error) {
-            logger.error(error);
-            console.log(error);
-            res.status(500).send('Sorry, but something happend on our server!');
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(result);
-            logger.info('Succesfully sent', result);
-        }
+        cbFunction(error, result, res, logger);
     });
 });
 
 routes.get('/flights', (req, res) => {
     flightDetailDao.getAll(function(error, result) {
-        if (error) {
-            logger.error(error);
-            console.log(error);
-            res.status(500).send('Sorry, but something happend on our server!');
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(result);
-            logger.info('Succesfully sent', result);
-        }
+        cbFunction(error, result, res, logger);
     });
 });
 
