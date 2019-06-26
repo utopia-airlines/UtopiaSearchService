b35f926166f88546a8d89f261000d51163fbddb1
Salem <SOzaki19@cornellcollege.edu>
2019-06-26 16:46:45 -0400

    condense one line function
    in airportDetailFilter

--- app/util/airportDetailFilter.js
+++ app/util/airportDetailFilter.js
@@ -1,4 +1,2 @@
 // pass in the airport code and the db connection (which includes the escape function)
-module.exports = (airportCode, db) => {
-    return db.escape(airportCode);
-};
+module.exports = (airportCode, db) => db.escape(airportCode);
