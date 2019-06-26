c64459245e0db3d22d8e035a21fd3c5afdfbb230
Salem <SOzaki19@cornellcollege.edu>
2019-06-25 21:55:20 -0400

    remove reusable code and put it into a function
    removed the callback function for the controller and put it into its own method

---
+++ app/controllers/controllerCallBack.js
@@ -0,0 +1,11 @@
+module.exports = (error, result, res, logger) => {
+    if (error) {
+        logger.error(error);
+        console.log(error);
+        res.status(500).send('Sorry, but something happend on our server!');
+    } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.send(result);
+        logger.info('Succesfully sent', result);
+    }
+}
