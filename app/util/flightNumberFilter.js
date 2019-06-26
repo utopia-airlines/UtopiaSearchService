554d3b414dcbb55be5a876c2f3cc7772831f4ebc
Salem <SOzaki19@cornellcollege.edu>
2019-06-26 16:56:53 -0400

    condense one line function
    in flightNumberFilter

--- app/util/flightNumberFilter.js
+++ app/util/flightNumberFilter.js
@@ -1,4 +1,2 @@
 // pass in the flight number and the db connection (which includes the escape function)
-module.exports = (flightNumber, db) => {
-    return db.escape(flightNumber);
-};
+module.exports = (flightNumber, db) => db.escape(flightNumber);
