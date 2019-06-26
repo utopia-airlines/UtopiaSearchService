e3a54dded05802b70cf8bbbdf3c7ca871620ef7c
Salem <SOzaki19@cornellcollege.edu>
2019-06-25 21:55:20 -0400

    remove reusable code and put it into a function
    removed the callback function for the controller and put it into its own method

--- app/controllers/searchController.js
+++ app/controllers/searchController.js
@@ -1,19 +1,12 @@
 const searchDao = require('../dao/searchDao');
 const routes = require('express').Router();
 const logger = require('../util/controllerLogger').createLogger;
+const cbFunction = require('./controllerCallBack');
 
 routes.get('/', function(req, res) {
 
     searchDao.get(req.query, function(error, result) {
-        if (error) {
-            logger.error(error);
-            console.log(error);
-            res.status(500).send('Sorry, but something happend on our server!');
-        } else {
-            res.setHeader('Content-Type', 'application/json');
-            res.send(result);
-            logger.info('Succesfully sent', result);
-        }
+        cbFunction(error, result, res, logger);
     });
 });
 
