3153f1fccf76150c765f1757a3a626d99d69f4c2
Salem <SOzaki19@cornellcollege.edu>
2019-06-26 16:55:15 -0400

    indented line for style
    in the seatDetailDao

--- app/dao/seatDetailDao.js
+++ app/dao/seatDetailDao.js
@@ -4,6 +4,6 @@ const db = require('../config/db.config.prod');
 
 exports.get = function(filter, cb) {
     let sqlQuery = 'SELECT seat_row, seat FROM tbl_tickets AS t LEFT JOIN tbl_flights AS f ON t.flight = f.id WHERE f.flight_number = ' +
-    flightNumberFilter(filter.flight, db) + ';';
+        flightNumberFilter(filter.flight, db) + ';';
     dbConn.query(sqlQuery, cb);
 };
