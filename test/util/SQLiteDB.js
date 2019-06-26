b9e5e14c5600dab8751c77a96e27814ff0f59781
Salem <SOzaki19@cornellcollege.edu>
2019-06-25 21:22:24 -0400

    if a table exists, then do not create one
    this ensures that it does not try to create nor insert any new info unless it does not exist

--- test/util/SQLiteDB.js
+++ test/util/SQLiteDB.js
@@ -6,14 +6,17 @@ const fs = require('fs');
 module.exports.createSQLiteDatabase = function(done) {
     // Please look at the README for debugging to the console
     const Testdb = new sqliteDatabase('airlinesTest.db', {memory: true});
-    // creates a stream to listen to using a given file
-    const readInterface = readline.createInterface({
-        input: fs.createReadStream('test/resources/SQLiteDB.sql'),
-    });
-    // for each line, run that line's sql command
-    readInterface.on('line', function(line) {
-        Testdb.prepare(line).run();
-    });
+    // if the table does not exist, then create the tables. Otherwise, do not create it again
+    if(!(Testdb.prepare('SELECT name FROM sqlite_master WHERE type=\'table\' AND name=\'tbl_airports\';').get())) {
+        // creates a stream to listen to using a given file
+        const readInterface = readline.createInterface({
+            input: fs.createReadStream('test/resources/SQLiteDB.sql'),
+        });
+        // for each line, run that line's sql command
+        readInterface.on('line', function(line) {
+            Testdb.prepare(line).run();
+        });
+    }
 
     // let the tests know that the database has finished setting up
     done();
