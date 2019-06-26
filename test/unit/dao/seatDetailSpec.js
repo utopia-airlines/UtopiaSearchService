4921c33f413aa09b9ba645ed060514e03c667562
Salem <SOzaki19@cornellcollege.edu>
2019-06-26 15:00:21 -0400

    improve description for seatDetailSpec 'it' test

--- test/unit/dao/seatDetailSpec.js
+++ test/unit/dao/seatDetailSpec.js
@@ -8,7 +8,7 @@ describe('seatDetailDao', () => {
         sqliteUtil.createSQLiteDatabase(done);
     });
     describe('getting seat details using a flight number', () => {
-        it('it should return a list of 6 seat details given 154681', (done) => {
+        it('it should return a list of 9 seat details given 154681', (done) => {
             let filter = {flight: '154681'};
             seatDetailDao.get(filter, (err, result) => {
                 result.length.should.equal(9);
