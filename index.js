a924dd407f053bca44a930ee13b62bbfd6ef656c
Salem <SOzaki19@cornellcollege.edu>
2019-06-28 14:01:25 -0400

    airport and its details has been added
    it works if you setup API gateway with the same uris has their respective paths in the express one

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
 const searchController = require('./app/controllers/searchController');
+const airportDetailsController = require('./app/controllers/airportDetailsController');
+
 exports.handler = async (event) => {
     // if there is nothing in querystring, then just pass an empty obj instead of undefined
     const filter = (event && event.params && event.params.querystring) ? event.params.querystring : {};
-    console.log('below is the filter');
-    console.log(filter);
-
-  const promise = new Promise(function(resolve, reject) {
-        searchController.get(filter, (err,result) => {
-            if(err) {
-                console.log(err);
-                reject(Error(err));
-            } else {
-                resolve(result);
-            }
+    const path = event.context["resource-path"];
+    if (path === '/search') {
+        return new Promise(function(resolve, reject) {
+            searchController.get(filter, (err,result) => {
+                if(err) {
+                    console.log(err);
+                    reject(Error(err));
+                } else {
+                    resolve(result);
+                }
+            });
+      });
+    } else if (path === '/airports') {
+        return new Promise(function(resolve, reject) {
+            airportDetailsController.getAll((err,result) => {
+                if(err) {
+                    console.log(err);
+                    reject(Error(err));
+                } else {
+                    resolve(result);
+                }
+            });
         });
-  });
-  return promise
+    } else if (path === '/airportDetails') {
+        return new Promise(function(resolve, reject) {
+            airportDetailsController.getDetail(filter, (err,result) => {
+                if(err) {
+                    console.log(err);
+                    reject(Error(err));
+                } else {
+                    resolve(result);
+                }
+            });
+        });
+    }
 };
