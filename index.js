772738684bb484e98d224a0555a27d13566da301
Salem <SOzaki19@cornellcollege.edu>
2019-07-01 13:45:31 -0400

    replace if statements with switch statement
    switch statement is easier to read

--- index.js
+++ index.js
@@ -7,71 +7,74 @@ exports.handler = async (event) => {
     // if there is nothing in querystring, then just pass an empty obj instead of undefined
     const filter = (event && event.params && event.params.querystring) ? event.params.querystring : {};
     const path = event.context["resource-path"];
-    if (path === '/search') {
-        return new Promise(function(resolve, reject) {
-            searchController.get(filter, (err,result) => {
-                if(err) {
-                    console.log(err);
-                    reject(Error(err));
-                } else {
-                    resolve(result);
-                }
+    switch(path) {
+        case '/search':
+            return new Promise(function(resolve, reject) {
+                searchController.get(filter, (err,result) => {
+                    if(err) {
+                        console.log(err);
+                        reject(Error(err));
+                    } else {
+                        resolve(result);
+                    }
+                });
+          });
+        case '/airports':
+            return new Promise(function(resolve, reject) {
+                airportDetailsController.getAll((err,result) => {
+                    if(err) {
+                        console.log(err);
+                        reject(Error(err));
+                    } else {
+                        resolve(result);
+                    }
+                });
             });
-      });
-    } else if (path === '/airports') {
-        return new Promise(function(resolve, reject) {
-            airportDetailsController.getAll((err,result) => {
-                if(err) {
-                    console.log(err);
-                    reject(Error(err));
-                } else {
-                    resolve(result);
-                }
+        case '/airportDetails':
+            return new Promise(function(resolve, reject) {
+                airportDetailsController.getDetail(filter, (err,result) => {
+                    if(err) {
+                        console.log(err);
+                        reject(Error(err));
+                    } else {
+                        resolve(result);
+                    }
+                });
             });
-        });
-    } else if (path === '/airportDetails') {
-        return new Promise(function(resolve, reject) {
-            airportDetailsController.getDetail(filter, (err,result) => {
-                if(err) {
-                    console.log(err);
-                    reject(Error(err));
-                } else {
-                    resolve(result);
-                }
+        case '/flights':
+            return new Promise(function(resolve, reject) {
+                flightDetailsController.getAll((err,result) => {
+                    if(err) {
+                        console.log(err);
+                        reject(Error(err));
+                    } else {
+                        resolve(result);
+                    }
+                });
             });
-        });
-    } else if (path === '/flights') {
-        return new Promise(function(resolve, reject) {
-            flightDetailsController.getAll((err,result) => {
-                if(err) {
-                    console.log(err);
-                    reject(Error(err));
-                } else {
-                    resolve(result);
-                }
+        case '/flightDetails':
+            return new Promise(function(resolve, reject) {
+                flightDetailsController.getDetail(filter, (err,result) => {
+                    if(err) {
+                        console.log(err);
+                        reject(Error(err));
+                    } else {
+                        resolve(result);
+                    }
+                });
             });
-        });
-    } else if (path === '/flightDetails') {
-        return new Promise(function(resolve, reject) {
-            flightDetailsController.getDetail(filter, (err,result) => {
-                if(err) {
-                    console.log(err);
-                    reject(Error(err));
-                } else {
-                    resolve(result);
-                }
+        case '/seats':
+            return new Promise(function(resolve, reject) {
+                seatDetailsController.getDetail(filter, (err,result) => {
+                    if(err) {
+                        console.log(err);
+                        reject(Error(err));
+                    } else {
+                        resolve(result);
+                    }
+                });
             });
-        });
-    } else if (path === '/seats') {
-        return new Promise(function(resolve, reject) {
-            seatDetailsController.getDetail(filter, (err,result) => {
-                if(err) {
-                    console.log(err);
-                    reject(Error(err));
-                } else {
-                    resolve(result);
-                }
-            });
-        });
+        default:
+            return null;
     }
 };
