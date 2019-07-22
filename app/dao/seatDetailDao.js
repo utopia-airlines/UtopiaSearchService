a553237c44bb5da1314c35c93047796f2328a12e
Jonathan Lovelace <kingjon3377@gmail.com>
2019-07-22 14:23:34 -0400

    Make get-seats-for-flight endpoint return seat class and whether it is reserved as well as its coordinates.
    By the API definition we should also return the flight (object), but
    as that would require another SELECT I'll postpone that to another
    PR.

--- app/dao/seatDetailDao.js
+++ app/dao/seatDetailDao.js
@@ -2,8 +2,29 @@ const flightNumberFilter = require('../util/flightNumberFilter');
 const dbConn = require('./db');
 const db = require('../config/db.config.prod');
 
+function booleanConvertingWrapper(cb) {
+    return function(err, result) {
+        if (err) {
+            cb(err, result);
+        } else {
+            for (record of result) {
+                for (key in record) {
+                    if (record[key] === 'true') {
+                        record[key] = true;
+                    } else if (record[key] === 'false') {
+                        record[key] = false;
+                    } else if (key === 'reserved') {
+                        console.log(record);
+                    }
+                }
+            }
+            cb(err, result);
+        }
+    }
+}
+
 exports.get = function(filter, cb) {
-    let sqlQuery = 'SELECT seat_row, seat FROM tbl_tickets AS t LEFT JOIN tbl_flights AS f ON t.flight = f.id WHERE f.flight_number = ' +
+    let sqlQuery = 'SELECT seat_row, seat, class, CASE WHEN reserver IS NULL THEN \'false\' ELSE \'true\' END AS reserved FROM tbl_tickets AS t LEFT JOIN tbl_flights AS f ON t.flight = f.id WHERE f.flight_number = ' +
         flightNumberFilter(filter.flight, db) + ';';
-    dbConn.query(sqlQuery, cb);
+    dbConn.query(sqlQuery, booleanConvertingWrapper(cb));
 };
