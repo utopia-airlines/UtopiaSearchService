437e2faa1786858ea142ad69a785de412460c11e
Jonathan Lovelace <kingjon3377@gmail.com>
2019-08-30 16:41:38 -0400

    Make seat-details query return flight details, including full airports, in nested objects.
    As with search results, this is required (for user-facing API callers) by
    the API spec. And also as with search results, this commit does not change
    any code path exercised by the tests, so I do not have complete confidence
    that this is correct, and I note that tests should be extended (later) to
    cover the details of what a seat-details query should return.

--- app/dao/seatDetailDao.js
+++ app/dao/seatDetailDao.js
@@ -1,6 +1,8 @@
 const flightNumberFilter = require('../util/flightNumberFilter');
 const dbConn = require('./db');
 const db = require('../config/db.config.prod');
+const nestAirportsWrapper = require('../util/nestAirportsWrapper');
+const nestFlightWrapper = require('../util/nestFlightWrapper');
 
 function booleanConvertingWrapper(cb) {
     return function(err, result) {
@@ -21,11 +23,20 @@ function booleanConvertingWrapper(cb) {
 
 exports.get = function(filter, cb) {
     let sqlQuery = `SELECT seat_row, seat, class,
+                        departure_airport.code AS departure_code,
+                        departure_airport.name AS departure_name,
+                        destination_airport.code AS destination_code,
+                        destination_airport.name AS destination_name,
+                        departure_date, arrival_date, flight_number,
                         CASE WHEN reserver IS NULL THEN 'false' ELSE 'true' END
                             AS reserved
                         FROM tbl_tickets AS t
                         LEFT JOIN tbl_flights AS f ON t.flight = f.id
+                        LEFT JOIN tbl_airports departure_airport
+                            ON f.departure = departure_airport.code
+                        LEFT JOIN tbl_airports destination_airport
+                            ON f.destination = destination_airport.code
                         WHERE f.flight_number = ${flightNumberFilter(filter.flight, db)}
                         ;`;
-    dbConn.query(sqlQuery, booleanConvertingWrapper(cb));
+    dbConn.query(sqlQuery, nestFlightWrapper(nestAirportsWrapper(booleanConvertingWrapper(cb))));
 };
